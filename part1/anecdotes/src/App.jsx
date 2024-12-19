6f29f25827d1eb385d4ee6d5ceca283f62c01bf0
optimisticnpc <joshuajltan@gmail.com>
2024-12-20 08:47:35 +1300

    Finished 1-14

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -36,12 +36,17 @@ const App = () => {
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {anecdotes[selected]}
       <br/>
       has {votes[selected]} votes
       <br/>
       <Button onClick={() => increaseVote(selected)} text="vote" />
       <Button onClick={() => setSelected(randomNumber())} text="next anecdote" />
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[votes.indexOf(Math.max(...votes))]}
+      <br/>
+      has {Math.max(...votes)} votes
     </div>
   )
 }
