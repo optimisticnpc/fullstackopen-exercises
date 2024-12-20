8a66be6c0d07ad943d9c65c2c1e78978435603ba
optimisticnpc <joshuajltan@gmail.com>
2024-12-21 12:39:00 +1300

    Finished 2-3

--- part2/courseinfo/src/Total.jsx
+++ part2/courseinfo/src/Total.jsx
@@ -1,6 +1,7 @@
 const Total = (props) => {
     return (
         <div>
+            {/* Reduce is used */}
             <p><b>total of {props.parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</b></p>
         </div>
     )
